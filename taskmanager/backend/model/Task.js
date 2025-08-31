b16c17f08917c8785738eb4de49c6f3aacc0e1bd
juliusagbo <128760135+juliusagbo@users.noreply.github.com>
2025-08-31 13:44:10 +0800

    Add files via upload

---
+++ taskmanager/backend/model/Task.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
